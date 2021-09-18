f0617f4dc47c33e907630eed5dbca8bf8f9a0e34
andarweb <andarweb@gmail.com>
2021-09-18 15:27:18 +0700

    create addUser test case

--- src/test/java/com/fullstack/devops/TestWebApp.java
+++ src/test/java/com/fullstack/devops/TestWebApp.java
@@ -31,6 +31,12 @@ public class TestWebApp extends DevopsApplicationTests {
 		.andExpect(status().isOk())
 		.andExpect(view().name("list-users"));
 	}
-	
+
+	@Test
+	public void testAddUser() throws Exception {
+		mockMvc.perform(get("/api/add"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("add-user"));
+	}
 	
 }
